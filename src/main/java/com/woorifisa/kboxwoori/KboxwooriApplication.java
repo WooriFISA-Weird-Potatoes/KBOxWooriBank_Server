112d8116a20833676cc0be223b533fa60818fb3f
Dain Lee <dain9934@gmail.com>
2023-08-20 18:06:23 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/woorifisa/kboxwoori/KboxwooriApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.kboxwoori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboxwooriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KboxwooriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorifisa/kboxwoori/KboxwooriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.kboxwoori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KboxwooriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
