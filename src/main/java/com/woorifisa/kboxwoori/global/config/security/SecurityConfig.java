b366eb86c42a9e06b9278a3aa32824eddaed062a
2un-light <sts07129@naver.com>
2023-08-24 19:05:52 +0900

    feat: login 설정 추가

--- src/main/java/com/woorifisa/kboxwoori/global/config/security/SecurityConfig.java
+++ src/main/java/com/woorifisa/kboxwoori/global/config/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.woorifisa.kboxwoori.global.config.security;
 
+import com.woorifisa.kboxwoori.domain.user.service.PrincipalDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,17 +17,36 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PrincipalDetailService principalDetailService;
+
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(principalDetailService).passwordEncoder(encoder());
     }
 
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/join").permitAll()
-                .anyRequest()
-                .authenticated();
+                .antMatchers("/api/join", "/api/login").permitAll()
+                .antMatchers("/api/admin/**").access("hasRole(ROLE_ADMIN)")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/api/login")
+                .usernameParameter("userId")
+                .passwordParameter("password")
+                .loginPage("/api/loginform")
+                .defaultSuccessUrl("/")
+                .failureUrl("/api/loginfail")
+                .and()
+                .logout()
+                .logoutUrl("/api/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true).deleteCookies("JSESSIONID");
     }
 }
