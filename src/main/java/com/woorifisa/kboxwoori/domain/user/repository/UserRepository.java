d0c4772c6bd7fa045fe0f9b6ff790b302ee957bd
2un-light <sts07129@naver.com>
2023-08-25 14:58:06 +0900

    feat: userId 기반 삭제 메서드 커스텀

--- src/main/java/com/woorifisa/kboxwoori/domain/user/repository/UserRepository.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/repository/UserRepository.java
@@ -4,10 +4,11 @@ import com.woorifisa.kboxwoori.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userID);
+    Optional<User> deleteByUserId(String UserID);
 }
