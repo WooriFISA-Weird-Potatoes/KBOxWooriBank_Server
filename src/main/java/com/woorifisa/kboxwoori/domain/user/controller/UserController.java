7a528337945ca2d6e4f20fcdc710634e3f0e757e
2un-light <sts07129@naver.com>
2023-08-25 20:04:04 +0900

    feat: 회원탈퇴 시 검증 로직 추가 및 회원정보 수정 api 추가

--- src/main/java/com/woorifisa/kboxwoori/domain/user/controller/UserController.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.woorifisa.kboxwoori.domain.user.controller;
 
+import com.woorifisa.kboxwoori.domain.user.dto.UpdateUserResponseDTO;
 import com.woorifisa.kboxwoori.domain.user.dto.UserDTO;
 import com.woorifisa.kboxwoori.domain.user.service.UserService;
 import com.woorifisa.kboxwoori.global.config.security.PrincipalDetails;
@@ -8,6 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
@@ -29,16 +31,34 @@ public class UserController {
     }
 
     @DeleteMapping("/api/users")
-    public String cancleMembership(@AuthenticationPrincipal PrincipalDetails pdtails, HttpSession session){
+    public String cancleMembership(@AuthenticationPrincipal PrincipalDetails pdetails, HttpServletRequest request){
 
-        boolean result = userService.deleteUser(pdtails.getUsername());
+        boolean result = userService.deleteUser(pdetails.getUsername());
+
         if(result){
+            HttpSession session = request.getSession(false);
             SecurityContextHolder.clearContext();
-            session.invalidate();
+            if(session != null){
+                session.invalidate();
+            }
             return "탈퇴 성공!";
         }
         return "탈퇴 처리에 실패하였습니다.";
     }
+
+    @GetMapping("/api/users")
+    public UpdateUserResponseDTO showEditForm(@AuthenticationPrincipal PrincipalDetails pdetails){
+        UpdateUserResponseDTO updateUserResponseDTO = userService.findUser(pdetails.getUsername());
+        return updateUserResponseDTO;
+    }
+
+    @PutMapping("/api/users")
+    public String EditUserInfo(@AuthenticationPrincipal PrincipalDetails pdetails, @RequestBody UpdateUserResponseDTO userDto){
+        UpdateUserResponseDTO updateUserResponseDTO = userService.updateUser(pdetails.getUsername(), userDto);
+        return "수정 성공!";
+    }
+
+
 
 
 }
