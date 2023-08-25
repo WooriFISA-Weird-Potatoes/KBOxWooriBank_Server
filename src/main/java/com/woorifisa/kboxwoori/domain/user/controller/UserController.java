1d33e8bdba84cc8b188090d10d2f11ee59753978
2un-light <sts07129@naver.com>
2023-08-25 15:00:04 +0900

    feature: 탈퇴 api 추가

--- src/main/java/com/woorifisa/kboxwoori/domain/user/controller/UserController.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
