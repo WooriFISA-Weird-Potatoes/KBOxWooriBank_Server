5bc7d772c5a0d023a45f576cfae46e71bb3f9037
2un-light <sts07129@naver.com>
2023-08-25 20:04:35 +0900

    feat: 회원정보 수정 메서드 updateUser 추가

--- src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.woorifisa.kboxwoori.domain.user.service;
 
+import com.woorifisa.kboxwoori.domain.user.dto.UpdateUserResponseDTO;
 import com.woorifisa.kboxwoori.domain.user.dto.UserDTO;
+import com.woorifisa.kboxwoori.domain.user.dto.UserSessionDTO;
 import com.woorifisa.kboxwoori.domain.user.entity.User;
 import com.woorifisa.kboxwoori.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,22 +10,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
 
-    @Transactional
+
     public Long join(UserDTO userDto) {
         userDto.setPassword(encoder.encode(userDto.getPassword()));
         return userRepository.save(userDto.toEntity()).getId();
     }
 
-    @Transactional
     public boolean deleteUser(String userId) {
         Optional<User> user = userRepository.findByUserId(userId);
 
@@ -32,5 +36,34 @@ public class UserService {
         });
         return true;
     }
+
+    public UpdateUserResponseDTO findUser(String userId) {
+        User user = userRepository.findByUserId(userId).get();
+        UpdateUserResponseDTO updateUserResponseDTO = new UpdateUserResponseDTO(user);
+
+        if(user != null){
+            return updateUserResponseDTO;
+        }
+        return null;
+    }
+
+    @Transactional
+    public UpdateUserResponseDTO updateUser(String userId, UpdateUserResponseDTO responseDTO){
+        User user = userRepository.findByUserId(userId).get();
+        user.updateUser(responseDTO.getUserId(),
+                        encoder.encode(responseDTO.getPassword()),
+                        responseDTO.getName(),
+                        responseDTO.getGender(),
+                        responseDTO.getBirth(),
+                        responseDTO.getPhone(),
+                        responseDTO.getAddr(),
+                        responseDTO.getClub());
+
+        return responseDTO;
+    }
+
+
+
+
 
 }
