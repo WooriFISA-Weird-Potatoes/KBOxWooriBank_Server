0150ee67d24edbc08bf3d4ffbb7175a85cda2167
2un-light <sts07129@naver.com>
2023-08-25 14:58:22 +0900

    feat: deleteUser 메서드 추가

--- src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
@@ -1,12 +1,14 @@
 package com.woorifisa.kboxwoori.domain.user.service;
 
 import com.woorifisa.kboxwoori.domain.user.dto.UserDTO;
+import com.woorifisa.kboxwoori.domain.user.entity.User;
 import com.woorifisa.kboxwoori.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,5 +23,14 @@ public class UserService {
         return userRepository.save(userDto.toEntity()).getId();
     }
 
+    @Transactional
+    public boolean deleteUser(String userId) {
+        Optional<User> user = userRepository.findByUserId(userId);
+
+        user.ifPresent(selectUser ->{
+            userRepository.deleteByUserId(userId);
+        });
+        return true;
+    }
 
 }
