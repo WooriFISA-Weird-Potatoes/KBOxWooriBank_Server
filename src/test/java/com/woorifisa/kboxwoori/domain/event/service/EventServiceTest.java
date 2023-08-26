f67d75a65b9754d5afe8d9106bb87d9e97f235a7
Dain Lee <dain9934@gmail.com>
2023-08-27 00:23:53 +0900

    test: 선착순 이벤트 테스트 코드 추가

---
+++ src/test/java/com/woorifisa/kboxwoori/domain/event/service/EventServiceTest.java
@@ -0,0 +1,111 @@
+package com.woorifisa.kboxwoori.domain.event.service;
+
+import com.woorifisa.kboxwoori.domain.event.entity.Event;
+import com.woorifisa.kboxwoori.domain.event.exception.OngoingEventNotFoundException;
+import com.woorifisa.kboxwoori.domain.event.repository.EventRepository;
+import com.woorifisa.kboxwoori.domain.user.entity.Club;
+import com.woorifisa.kboxwoori.domain.user.entity.Gender;
+import com.woorifisa.kboxwoori.domain.user.entity.User;
+import com.woorifisa.kboxwoori.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EventServiceTest {
+
+    @Autowired
+    private EventService eventService;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 오늘의_이벤트_가져오기() {
+        //진행중인 이벤트
+        Event event = Event.builder()
+                .prize("아이패드")
+                .winnerLimit(10)
+                .startDate(LocalDateTime.of(LocalDate.now(), LocalTime.MIN))
+                .endDate(LocalDateTime.of(LocalDate.now(), LocalTime.MAX))
+                .isEnded(false)
+                .build();
+
+        Event savedEvent = eventRepository.save(event);
+
+        //종료된 이벤트
+        Event event2 = Event.builder()
+                .prize("아이패드")
+                .winnerLimit(10)
+                .startDate(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.MIN))
+                .endDate(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.MAX))
+                .isEnded(false)
+                .build();
+
+        eventRepository.save(event2);
+
+        assertEquals(savedEvent.getId(), eventService.findCurrentEvent().getId());
+    }
+
+    @Test
+    void 오늘의_이벤트가_없으면_실패() {
+        //진행중인 이벤트
+        Event event = Event.builder()
+                .prize("아이패드")
+                .winnerLimit(10)
+                .startDate(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.MIN))
+                .endDate(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.MAX))
+                .isEnded(false)
+                .build();
+
+        eventRepository.save(event);
+
+        assertThrows(OngoingEventNotFoundException.class, () -> eventService.findCurrentEvent());
+    }
+
+    @Test
+    void 이벤트_시간_외에_참여하면_실패() {
+        User user = User.builder()
+                .admin(false)
+                .userId("test")
+                .password("test")
+                .name("test")
+                .gender(Gender.F)
+                .birth(LocalDate.now())
+                .phone("000-0000-0000")
+                .addr("seoul")
+                .club(Club.DOOSAN)
+                .point(0)
+                .svcAgmt(true)
+                .infoAgmt(true)
+                .wooriLinked(false)
+                .build();
+
+        userRepository.save(user);
+
+        //진행중인 이벤트
+        Event event = Event.builder()
+                .prize("아이패드")
+                .winnerLimit(10)
+                .startDate(LocalDateTime.of(LocalDate.now(), LocalTime.now().plusHours(1)))
+                .endDate(LocalDateTime.of(LocalDate.now(), LocalTime.now().plusHours(2)))
+                .isEnded(false)
+                .build();
+
+        eventRepository.save(event);
+
+        eventService.findCurrentEvent();
+        Boolean result = eventService.joinEvent("test");
+
+        assertFalse(result);
+    }
+}
