c91a30a1bb05a651092421deda0603639028f241
2un-light <sts07129@naver.com>
2023-08-28 14:21:29 +0900

    feat: 꿀머니 전환 페이지 사용자 포인트 반환 메서드

--- src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
@@ -1,8 +1,8 @@
 package com.woorifisa.kboxwoori.domain.user.service;
 
-import com.woorifisa.kboxwoori.domain.user.dto.UpdateUserResponseDTO;
+import com.woorifisa.kboxwoori.domain.user.dto.UserInfoResponseDTO;
 import com.woorifisa.kboxwoori.domain.user.dto.UserDTO;
-import com.woorifisa.kboxwoori.domain.user.dto.UserSessionDTO;
+import com.woorifisa.kboxwoori.domain.user.dto.UserPointResponseDTO;
 import com.woorifisa.kboxwoori.domain.user.entity.User;
 import com.woorifisa.kboxwoori.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,24 +10,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
 
-
+    @Transactional
     public Long join(UserDTO userDto) {
         userDto.setPassword(encoder.encode(userDto.getPassword()));
         return userRepository.save(userDto.toEntity()).getId();
     }
 
+    @Transactional
     public boolean deleteUser(String userId) {
         Optional<User> user = userRepository.findByUserId(userId);
 
@@ -36,10 +34,10 @@ public class UserService {
         });
         return true;
     }
-
-    public UpdateUserResponseDTO findUser(String userId) {
+    @Transactional
+    public UserInfoResponseDTO findUser(String userId) {
         User user = userRepository.findByUserId(userId).get();
-        UpdateUserResponseDTO updateUserResponseDTO = new UpdateUserResponseDTO(user);
+        UserInfoResponseDTO updateUserResponseDTO = new UserInfoResponseDTO(user);
 
         if(user != null){
             return updateUserResponseDTO;
@@ -48,20 +46,23 @@ public class UserService {
     }
 
     @Transactional
-    public UpdateUserResponseDTO updateUser(String userId, UpdateUserResponseDTO responseDTO){
+    public UserInfoResponseDTO updateUserInfo(String userId, UserInfoResponseDTO responseDTO){
         User user = userRepository.findByUserId(userId).get();
-        user.updateUser(responseDTO.getUserId(),
-                        encoder.encode(responseDTO.getPassword()),
-                        responseDTO.getName(),
-                        responseDTO.getGender(),
-                        responseDTO.getBirth(),
-                        responseDTO.getPhone(),
-                        responseDTO.getAddr(),
-                        responseDTO.getClub());
-
+        responseDTO.setPassword(encoder.encode(responseDTO.getPassword()));
+        user.updateUser(responseDTO);
         return responseDTO;
     }
 
+    @Transactional
+    public UserPointResponseDTO getUserPoint(String userId){
+        User user = userRepository.findPointByUserId(userId).get();
+
+        UserPointResponseDTO userPointResponseDTO = new UserPointResponseDTO();
+        userPointResponseDTO.setPoint(user.getPoint());
+
+        return userPointResponseDTO;
+
+    }
 
 
 
