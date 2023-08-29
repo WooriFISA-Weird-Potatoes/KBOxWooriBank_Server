5531a943c6f28ed28f382958c3f1ceecec63c531
Dain Lee <dain9934@gmail.com>
2023-08-29 15:13:50 +0900

    feat: PredictionHistory Repository 생성

---
+++ src/main/java/com/woorifisa/kboxwoori/domain/prediction/repository/PredictionHistoryRepository.java
@@ -0,0 +1,18 @@
+package com.woorifisa.kboxwoori.domain.prediction.repository;
+
+import com.woorifisa.kboxwoori.domain.prediction.entity.PredictionHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PredictionHistoryRepository extends JpaRepository<PredictionHistory, Long> {
+    @Query(value = "SELECT p FROM PredictionHistory p WHERE p.user.userId = :userId")
+    Optional<List<PredictionHistory>> findByUserId(String userId);
+
+    @Query(value = "SELECT p FROM PredictionHistory p WHERE p.user.userId = :userId AND p.isCorrect = true")
+    Optional<List<PredictionHistory>> findByUserIdAndIsCorrect(String userId);
+}
