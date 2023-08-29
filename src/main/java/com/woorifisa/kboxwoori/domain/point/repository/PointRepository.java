11c647798bacc87c92cc26e650ae5a1349946007
2un-light <sts07129@naver.com>
2023-08-29 12:19:28 +0900

    bugfix: id값에 해당하는 회원의 포인트 내역 삭제

--- src/main/java/com/woorifisa/kboxwoori/domain/point/repository/PointRepository.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/point/repository/PointRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
     @Query("SELECT ph FROM Point ph WHERE ph.user.userId = :userId ORDER BY ph.id DESC")
     List<Point> findPointByUserId(String userId);
+    Optional<Point> deletePointByUserId(Long id);
+
+    
 
 }
