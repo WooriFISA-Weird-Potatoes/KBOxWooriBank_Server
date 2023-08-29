b99a3cc7df8fb1e7c5fbb9ac98dc83693566e751
2un-light <sts07129@naver.com>
2023-08-29 19:53:49 +0900

    feat: 회원정보 수정 시 필드값 검증

--- src/main/java/com/woorifisa/kboxwoori/domain/user/dto/UserInfoResponseDto.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/dto/UserInfoResponseDto.java
@@ -7,18 +7,36 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 @Getter
 @AllArgsConstructor
 @Data
 public class UserInfoResponseDto {
+    @NotNull(message = "변경할 아이디를 입력해주세요.")
     private String userId;
+
+    @NotNull(message = "변경할 비밀번호를 입력해주세요.")
     private String password;
+
+    @NotNull(message = "변경할 이름을 입력해주세요.")
     private String name;
+
+    @NotNull(message = "변경할 성별을 선택해주세요.")
     private Gender gender;
+
+    @NotNull(message = "변경할 생년월일을 입력해주세요.")
     private LocalDate birth;
+
+    @NotNull(message = "변경할 휴대폰 번호를 입력해주세요.")
+    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
     private String phone;
+
+    @NotNull(message = "변경할 주소를 입력해주세요.")
     private String addr;
+
+    @NotNull(message = "변경할 구단을 선택해주세요.")
     private Club club;
 
     public UserInfoResponseDto(User user) {
