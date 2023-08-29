808b9effe19dd174088a43627f4d310c549a2eaa
2un-light <sts07129@naver.com>
2023-08-29 12:39:43 +0900

    feat: 잔여 포인트 부족 예외처리 클래스

---
+++ src/main/java/com/woorifisa/kboxwoori/domain/point/exception/InsufficientPointsException.java
@@ -0,0 +1,12 @@
+package com.woorifisa.kboxwoori.domain.point.exception;
+
+import com.woorifisa.kboxwoori.global.exception.CustomException;
+import com.woorifisa.kboxwoori.global.exception.CustomExceptionStatus;
+
+public class InsufficientPointsException extends CustomException {
+    public static final CustomException EXCEPTION = new InsufficientPointsException();
+
+    private InsufficientPointsException(){
+        super(CustomExceptionStatus.POINT_OPERAION_ERROR);
+    }
+}
