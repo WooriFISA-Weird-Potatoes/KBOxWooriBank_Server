fc45d32accaef4d87c7555dc4ba6012db1d49ac0
2un-light <sts07129@naver.com>
2023-08-29 14:06:09 +0900

    feat: 꿀머니 전환 시 사용자 포인트 조회 및 포인트 사용 api

--- src/main/java/com/woorifisa/kboxwoori/domain/point/controller/PointController.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/point/controller/PointController.java
@@ -1,23 +1,40 @@
 package com.woorifisa.kboxwoori.domain.point.controller;
 
-import com.woorifisa.kboxwoori.domain.point.dto.PointHistoryDTO;
+import com.woorifisa.kboxwoori.domain.point.dto.PointHistoryDto;
+import com.woorifisa.kboxwoori.domain.point.dto.UserPointResponseDto;
 import com.woorifisa.kboxwoori.domain.point.service.PointService;
 import com.woorifisa.kboxwoori.global.config.security.PrincipalDetails;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.woorifisa.kboxwoori.global.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PointController {
 
-    @Autowired
-    PointService pointService;
+    private final PointService pointService;
 
     @GetMapping("/api/point")
-    public PointHistoryDTO getPointHistory(@AuthenticationPrincipal PrincipalDetails pdetails){
-        PointHistoryDTO pointHistoryDTO = pointService.getPointHistory(pdetails.getUsername());
-        return pointHistoryDTO;
+    public ResponseDto<PointHistoryDto> getPointHistory(@AuthenticationPrincipal PrincipalDetails pdetails){
+        PointHistoryDto pointHistoryDTO = pointService.getPointHistory(pdetails.getUsername());
+        return ResponseDto.success(pointHistoryDTO);
+    }
+
+    @GetMapping("/api/point/honeymoney")
+    public ResponseDto<UserPointResponseDto> getUserPoint(@AuthenticationPrincipal PrincipalDetails pdetails){
+        UserPointResponseDto userPointResponseDTO = pointService.getUserPoint(pdetails.getUsername());
+        return ResponseDto.success(userPointResponseDTO);
+    }
+
+    @PostMapping("/api/point/honeymoney")
+    public ResponseDto usePoint(@AuthenticationPrincipal PrincipalDetails pdetails, @RequestBody UserPointResponseDto userPointResponseDTO){
+        pointService.usePoint(pdetails.getUsername(), userPointResponseDTO);
+        return ResponseDto.success();
+
     }
 
 
