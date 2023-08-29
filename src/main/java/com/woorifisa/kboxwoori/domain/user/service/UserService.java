cf12152363a8fcb9d101640d820bdaf7f0c13be3
2un-light <sts07129@naver.com>
2023-08-29 12:21:26 +0900

    bugfix: 회원 탈퇴 처리 로직에 해당 회원의 포인트 내역 삭제하는 로직 추가

--- src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/service/UserService.java
@@ -1,69 +1,55 @@
 package com.woorifisa.kboxwoori.domain.user.service;
 
 import com.woorifisa.kboxwoori.domain.event.exception.WooriLinkRequiredException;
-import com.woorifisa.kboxwoori.domain.user.dto.UserInfoResponseDTO;
-import com.woorifisa.kboxwoori.domain.user.dto.UserDTO;
-import com.woorifisa.kboxwoori.domain.user.dto.UserPointResponseDTO;
+import com.woorifisa.kboxwoori.domain.point.repository.PointRepository;
+import com.woorifisa.kboxwoori.domain.user.dto.UserInfoResponseDto;
+import com.woorifisa.kboxwoori.domain.user.dto.UserDto;
 import com.woorifisa.kboxwoori.domain.user.entity.User;
 import com.woorifisa.kboxwoori.domain.user.exception.AccountNotFoundException;
 import com.woorifisa.kboxwoori.domain.user.repository.UserRepository;
+import com.woorifisa.kboxwoori.global.config.security.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PointRepository pointRepository;
     private final BCryptPasswordEncoder encoder;
 
     @Transactional
-    public Long join(UserDTO userDto) {
+    public Long join(UserDto userDto) {
         userDto.setPassword(encoder.encode(userDto.getPassword()));
         return userRepository.save(userDto.toEntity()).getId();
     }
 
     @Transactional
     public boolean deleteUser(String userId) {
-        Optional<User> user = userRepository.findByUserId(userId);
-
-        user.ifPresent(selectUser ->{
-            userRepository.deleteByUserId(userId);
-        });
+        User user = userRepository.findByUserId(userId).orElseThrow(() -> AccountNotFoundException.EXCEPTION);
+        pointRepository.deletePointByUserId(user.getId());
+        userRepository.deleteByUserId(user.getUserId());
         return true;
     }
-    @Transactional
-    public UserInfoResponseDTO findUser(String userId) {
-        User user = userRepository.findByUserId(userId).get();
-        UserInfoResponseDTO updateUserResponseDTO = new UserInfoResponseDTO(user);
 
-        if(user != null){
-            return updateUserResponseDTO;
-        }
-        return null;
+    @Transactional
+    public UserInfoResponseDto findUser(String userId) {
+        User user = userRepository.findByUserId(userId).orElseThrow(() -> AccountNotFoundException.EXCEPTION);
+        UserInfoResponseDto updateUserResponseDTO = new UserInfoResponseDto(user);
+        return updateUserResponseDTO;
     }
 
     @Transactional
-    public UserInfoResponseDTO updateUserInfo(String userId, UserInfoResponseDTO responseDTO){
-        User user = userRepository.findByUserId(userId).get();
+    public UserInfoResponseDto updateUserInfo(PrincipalDetails pdetail, UserInfoResponseDto responseDTO){
+        User user = userRepository.findByUserId(pdetail.getUsername()).orElseThrow(() -> AccountNotFoundException.EXCEPTION);
         responseDTO.setPassword(encoder.encode(responseDTO.getPassword()));
         user.updateUser(responseDTO);
+        pdetail.setUser(user);
         return responseDTO;
-    }
-
-    @Transactional
-    public UserPointResponseDTO getUserPoint(String userId){
-        User user = userRepository.findPointByUserId(userId).get();
-
-        UserPointResponseDTO userPointResponseDTO = new UserPointResponseDTO();
-        userPointResponseDTO.setPoint(user.getPoint());
-
-        return userPointResponseDTO;
-
     }
 
     public Boolean IsWooriLinked(String userId) {
