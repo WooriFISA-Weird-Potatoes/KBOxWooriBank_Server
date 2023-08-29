c78724616d88638ac4a156207fa49922d665977f
Dain Lee <dain9934@gmail.com>
2023-08-29 17:37:00 +0900

    feat: 검증 예외 handler 추가

--- src/main/java/com/woorifisa/kboxwoori/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/woorifisa/kboxwoori/global/exception/CustomExceptionHandler.java
@@ -2,13 +2,30 @@ package com.woorifisa.kboxwoori.global.exception;
 
 import com.woorifisa.kboxwoori.global.response.ResponseDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class CustomExceptionHandler {
-    //TODO: validation error handler 추가
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseDto<?> validationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach((error) -> {
+                    errors.put(error.getField(), error.getDefaultMessage());
+                });
+        return ResponseDto.error(CustomExceptionStatus.REQUEST_ERROR, errors);
+    }
 
     @ExceptionHandler(CustomException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
