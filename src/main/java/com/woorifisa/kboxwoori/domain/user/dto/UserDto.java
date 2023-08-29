c16d0b456e889429e48341de70f0d9bb28a43f59
2un-light <sts07129@naver.com>
2023-08-29 14:14:11 +0900

    rename: DTO -> Dto 이름 변경

--- src/main/java/com/woorifisa/kboxwoori/domain/user/dto/UserDto.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/dto/UserDto.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserDTO {
+public class UserDto {
     private String userId;
     private String password;
     private String name;
