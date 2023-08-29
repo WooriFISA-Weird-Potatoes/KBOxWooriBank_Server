37a95a0aebef505e00fcd62e1c0aa3f0d0592707
2un-light <sts07129@naver.com>
2023-08-29 14:07:57 +0900

    feat: 포인트 이용 내역 Dto

---
+++ src/main/java/com/woorifisa/kboxwoori/domain/point/dto/PointUseDto.java
@@ -0,0 +1,31 @@
+package com.woorifisa.kboxwoori.domain.point.dto;
+
+import com.woorifisa.kboxwoori.domain.point.entity.Point;
+import com.woorifisa.kboxwoori.domain.point.entity.PointStatus;
+import com.woorifisa.kboxwoori.domain.user.entity.User;
+import lombok.*;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PointUseDto {
+    private User user;
+    private PointStatus statusCode;
+    private Integer point;
+    private LocalDate createdAt;
+
+    public Point toEntity(){
+        Point pointentity;
+        pointentity = Point.builder()
+                .user(user)
+                .statusCode(statusCode)
+                .point(point)
+                .createdAt(createdAt)
+                .build();
+        return pointentity;
+    }
+
+}
