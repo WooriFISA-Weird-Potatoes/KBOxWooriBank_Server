29bf875af77d7bd9188281c5948f43dcd36c4e09
2un-light <sts07129@naver.com>
2023-08-29 14:13:50 +0900

    refactor: 변수명 통일

--- src/main/java/com/woorifisa/kboxwoori/domain/user/controller/UserController.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/user/controller/UserController.java
@@ -1,78 +1,67 @@
 package com.woorifisa.kboxwoori.domain.user.controller;
 
-import com.woorifisa.kboxwoori.domain.user.dto.UserInfoResponseDTO;
-import com.woorifisa.kboxwoori.domain.user.dto.UserDTO;
-import com.woorifisa.kboxwoori.domain.user.dto.UserPointResponseDTO;
+import com.woorifisa.kboxwoori.domain.user.dto.UserInfoResponseDto;
+import com.woorifisa.kboxwoori.domain.user.dto.UserDto;
 import com.woorifisa.kboxwoori.domain.user.exception.NotAuthenticatedAccountException;
 import com.woorifisa.kboxwoori.domain.user.service.UserService;
 import com.woorifisa.kboxwoori.global.config.security.PrincipalDetails;
 import com.woorifisa.kboxwoori.global.response.ResponseDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
-    @Autowired
-    UserService userService;
+    private final UserService userService;
 
     @GetMapping({"", "/"})
-    public String index(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-        System.out.println(principalDetails.isWooriLinked());
+    public String index(@AuthenticationPrincipal PrincipalDetails pdetails) {
+        System.out.println(pdetails.isWooriLinked());
         return "메인페이지";
     }
 
     @PostMapping("/api/join")
-    public String join(@RequestBody UserDTO userDto){
+    public ResponseDto join(@RequestBody UserDto userDto){
         userService.join(userDto);
-        return "회원가입이 완료되었습니다!";
+        return ResponseDto.success();
     }
 
     @DeleteMapping("/api/users")
-    public String cancleMembership(@AuthenticationPrincipal PrincipalDetails pdetails, HttpServletRequest request){
-
+    public ResponseDto cancleMembership(@AuthenticationPrincipal PrincipalDetails pdetails, HttpServletRequest request){
         boolean result = userService.deleteUser(pdetails.getUsername());
-
         if(result){
             HttpSession session = request.getSession(false);
             SecurityContextHolder.clearContext();
             if(session != null){
                 session.invalidate();
             }
-            return "탈퇴 성공!";
         }
-        return "탈퇴 처리에 실패하였습니다.";
+        return ResponseDto.success();
     }
 
     @GetMapping("/api/users")
-    public UserInfoResponseDTO showEditForm(@AuthenticationPrincipal PrincipalDetails pdetails){
-        UserInfoResponseDTO updateUserResponseDTO = userService.findUser(pdetails.getUsername());
-        return updateUserResponseDTO;
+    public ResponseDto<UserInfoResponseDto> showEditForm(@AuthenticationPrincipal PrincipalDetails pdetails){
+        UserInfoResponseDto updateUserResponseDTO = userService.findUser(pdetails.getUsername());
+        return ResponseDto.success(updateUserResponseDTO);
     }
 
     @PutMapping("/api/users")
-    public UserInfoResponseDTO EditUserInfo(@AuthenticationPrincipal PrincipalDetails pdetails, @RequestBody UserInfoResponseDTO userDto){
-        UserInfoResponseDTO updateUserResponseDTO = userService.updateUserInfo(pdetails.getUsername(), userDto);
-        return updateUserResponseDTO;
-    }
-
-    @GetMapping("/api/point/honeymoney")
-    public UserPointResponseDTO getUserPoint(@AuthenticationPrincipal PrincipalDetails pdetails){
-        UserPointResponseDTO userPointResponseDTO = userService.getUserPoint(pdetails.getUsername());
-        return userPointResponseDTO;
+    public ResponseDto<UserInfoResponseDto> EditUserInfo(@AuthenticationPrincipal PrincipalDetails pdetails, @RequestBody UserInfoResponseDto userDto){
+        UserInfoResponseDto updateUserResponseDTO = userService.updateUserInfo(pdetails, userDto);
+        return ResponseDto.success(updateUserResponseDTO);
     }
 
     @GetMapping("/api/users/woori")
-    public ResponseDto IsWooriLinked(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-        if (principalDetails == null) {
+    public ResponseDto IsWooriLinked(@AuthenticationPrincipal PrincipalDetails pdetails) {
+        if (pdetails == null) {
             throw NotAuthenticatedAccountException.EXCEPTION;
         }
-        userService.IsWooriLinked(principalDetails.getUsername());
+        userService.IsWooriLinked(pdetails.getUsername());
         return ResponseDto.success();
     }
     
