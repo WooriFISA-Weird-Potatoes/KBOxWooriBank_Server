7b76a31a8b675411d0d06406d34cb66c05935811
YOUNGMIN <kooonym@gmail.com>
2023-08-29 23:08:55 +0900

    feat: TodayScheduleDto 생성

---
+++ src/main/java/com/woorifisa/kboxwoori/domain/crawling/dto/TodayScheduleDto.java
@@ -0,0 +1,59 @@
+package com.woorifisa.kboxwoori.domain.crawling.dto;
+
+import com.woorifisa.kboxwoori.domain.crawling.entity.TodaySchedule;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TodayScheduleDto {
+    private String id;
+    private LocalDate date;
+    private String gameTime;
+    private String team1Name;
+    private String team1Logo;
+    private String team1Score;
+    private String team1SPState;
+    private String team1SPLink;
+    private String team1SPName;
+    private String team2Name;
+    private String team2Logo;
+    private String team2Score;
+    private String team2SPState;
+    private String team2SPLink;
+    private String team2SPName;
+    private String comparisonLink;
+    private String comparisonText;
+    private String tvBroadcastLink;
+    private String tvBroadcastText;
+    private String remarks;
+
+    public TodaySchedule toEntity() {
+        return TodaySchedule.builder()
+                            .id(this.id)
+                            .date(this.date)
+                            .gameTime(this.gameTime)
+                            .team1Name(this.team1Name)
+                            .team1Logo(this.team1Logo)
+                            .team1Score(this.team1Score)
+                            .team1SPState(this.team1SPState)
+                            .team1SPLink(this.team1SPLink)
+                            .team1SPName(this.team1SPName)
+                            .team2Name(this.team2Name)
+                            .team2Logo(this.team2Logo)
+                            .team2Score(this.team2Score)
+                            .team2SPState(this.team2SPState)
+                            .team2SPLink(this.team2SPLink)
+                            .team2SPName(this.team2SPName)
+                            .comparisonLink(this.comparisonLink)
+                            .comparisonText(this.comparisonText)
+                            .tvBroadcastLink(this.tvBroadcastLink)
+                            .tvBroadcastText(this.tvBroadcastText)
+                            .remarks(this.remarks)
+                            .build();
+    }
+}
