99d9347b7bcc054539c156e7251dafb4456f3c2e
2un-light <sts07129@naver.com>
2023-08-30 16:43:26 +0900

    refactor: 중복 제거

--- src/main/java/com/woorifisa/kboxwoori/domain/point/controller/PointController.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/point/controller/PointController.java
@@ -7,30 +7,28 @@ import com.woorifisa.kboxwoori.global.config.security.PrincipalDetails;
 import com.woorifisa.kboxwoori.global.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/point")
 public class PointController {
 
     private final PointService pointService;
 
-    @GetMapping("/api/point")
+    @GetMapping
     public ResponseDto<PointHistoryDto> getPointHistory(@AuthenticationPrincipal PrincipalDetails pdetails){
         PointHistoryDto pointHistoryDTO = pointService.getPointHistory(pdetails.getUsername());
         return ResponseDto.success(pointHistoryDTO);
     }
 
-    @GetMapping("/api/point/honeymoney")
+    @GetMapping("/honeymoney")
     public ResponseDto<UserPointResponseDto> getUserPoint(@AuthenticationPrincipal PrincipalDetails pdetails){
         UserPointResponseDto userPointResponseDTO = pointService.getUserPoint(pdetails.getUsername());
         return ResponseDto.success(userPointResponseDTO);
     }
 
-    @PostMapping("/api/point/honeymoney")
+    @PostMapping("/honeymoney")
     public ResponseDto usePoint(@AuthenticationPrincipal PrincipalDetails pdetails, @RequestBody UserPointResponseDto userPointResponseDTO){
         pointService.usePoint(pdetails.getUsername(), userPointResponseDTO);
         return ResponseDto.success();
