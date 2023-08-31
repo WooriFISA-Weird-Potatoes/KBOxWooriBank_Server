f9b067f7c90db06feffd50bdc6ceae6c90f3f2e9
YOUNGMIN <kooonym@gmail.com>
2023-08-31 21:01:51 +0900

    feat: 뉴스 검색을 위한 Override

--- src/main/java/com/woorifisa/kboxwoori/domain/crawling/repository/NewsRepository.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/crawling/repository/NewsRepository.java
@@ -1,9 +1,15 @@
 package com.woorifisa.kboxwoori.domain.crawling.repository;
 
 import com.woorifisa.kboxwoori.domain.crawling.entity.News;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 @EnableRedisRepositories
-public interface NewsRepository extends CrudRepository<News, String> {
+public interface NewsRepository extends CrudRepository<News, String>, PagingAndSortingRepository<News, String> {
+    @Override
+    List<News> findAll(Sort sort);
 }
