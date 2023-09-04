c91c91249c85ca35d637ac21fa0fe56954deafeb
YOUNGMIN <kooonym@gmail.com>
2023-09-04 10:35:38 +0900

    refactor: 일정 크롤링 Refactoring

--- src/main/java/com/woorifisa/kboxwoori/domain/crawling/service/ScheduleCrawlingService.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/crawling/service/ScheduleCrawlingService.java
@@ -1,9 +1,9 @@
 package com.woorifisa.kboxwoori.domain.crawling.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woorifisa.kboxwoori.domain.crawling.entity.Schedule;
 import com.woorifisa.kboxwoori.domain.crawling.entity.TeamLogo;
-import com.woorifisa.kboxwoori.domain.crawling.entity.TodaySchedule;
+import com.woorifisa.kboxwoori.domain.crawling.exception.CrawlingDataSaveException;
+import com.woorifisa.kboxwoori.domain.crawling.exception.CrawlingStoredDataNotFoundException;
 import com.woorifisa.kboxwoori.domain.crawling.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +12,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -27,27 +24,14 @@ import java.util.*;
 @RequiredArgsConstructor
 public class ScheduleCrawlingService {
 
-    private final RedisTemplate<String, String> redisTemplate;
     private final ScheduleRepository scheduleRepository;
-    private  HashOperations<String, String, Schedule> hashOps;
-    @PostConstruct
-    private void init() {
-        hashOps = redisTemplate.opsForHash();
-    }
-    private String formattedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMdd"));
+
+    private final String formattedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
     public void crawlKboSchedule() {
         try {
             Document document = Jsoup.connect("https://sports.news.naver.com/kbaseball/schedule/index").get();
             Elements games = document.select("div.sch_tb, div.sch_tb2");
-            Elements todayGames = document.select("sch_vs");
-
-            int todayGamesindex = 1;
-            for (int i = 1; i < todayGames.size(); i++) {
-                Element row = todayGames.get(i);
-                System.out.println("row = " + row);
-                Elements cells = row.select("");
-            }
 
             int gamesIndex = 1;
             for (Element game : games) {
@@ -72,17 +56,14 @@ public class ScheduleCrawlingService {
                         stadium = "";
                     }
                     if (!row.select(".suspended").isEmpty()) {
-                        remarks = "경기취소";
+                        time = "경기취소";
                     }
                     if (time.equals("-")){
                         remarks = "프로야구 경기가 없습니다.";
                     }
 
-                    System.out.println("team1 = " + team1);
-                    System.out.println("team2 = " + team2);
-
-                    Schedule result = scheduleRepository.save(Schedule.builder()
-                                                                      .id(formattedTime + "-" + gamesIndex)
+                    Schedule saveSchedule = scheduleRepository.save(Schedule.builder()
+                                                                      .id(formattedTime + "-" + String.format("%03d", gamesIndex))
                                                                       .date(date)
                                                                       .time(time)
                                                                       .teamInfo(teamInfo)
@@ -94,19 +75,29 @@ public class ScheduleCrawlingService {
                                                                       .team1Logo(TeamLogo.fromString(team1).getTeamLogoUrl())
                                                                       .team2Logo(TeamLogo.fromString(team2).getTeamLogoUrl())
                                                                       .build());
-                    System.out.println("result = " + result);
+                    log.info("전체 경기 일정 저장 !:" + saveSchedule);
                     gamesIndex++;
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw CrawlingDataSaveException.EXCEPTION;
         }
     }
 
     public List<Schedule> scheduleFindAll() {
-        List<Schedule> schedules = scheduleRepository.findAll(Sort.by("id"));
-        log.info("전체 경기일정 API 호출 !: " + schedules);
-        return schedules;
+        List<Schedule> scheduleResults = null;
+        try {
+            List<Schedule> schedules = scheduleRepository.findAll(Sort.by("id"));
+            scheduleResults = new ArrayList<>();
+            for (Schedule schedule : schedules) {
+                if (schedule.getId().contains(formattedTime)){
+                    scheduleResults.add(schedule);
+                }
+            }
+            log.info("전체 경기일정 API 호출 !: " + scheduleResults);
+        } catch (Exception e) {
+            throw CrawlingStoredDataNotFoundException.EXCEPTION;
+        }
+        return scheduleResults;
     }
 }
