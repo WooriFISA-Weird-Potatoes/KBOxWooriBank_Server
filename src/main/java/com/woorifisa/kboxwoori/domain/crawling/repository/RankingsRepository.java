4ec537c19226028bf0ab0a511680ba2d01057988
YOUNGMIN <kooonym@gmail.com>
2023-09-04 10:34:33 +0900

    refactor: 순위 크롤링 Refactoring

--- src/main/java/com/woorifisa/kboxwoori/domain/crawling/repository/RankingsRepository.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/crawling/repository/RankingsRepository.java
@@ -1,9 +1,15 @@
 package com.woorifisa.kboxwoori.domain.crawling.repository;
 
 import com.woorifisa.kboxwoori.domain.crawling.entity.Rankings;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 @EnableRedisRepositories
-public interface RankingsRepository extends CrudRepository<Rankings, String> {
+public interface RankingsRepository extends CrudRepository<Rankings, String>, PagingAndSortingRepository<Rankings, String> {
+    @Override
+    List<Rankings> findAll(Sort sort);
 }
--- src/main/java/com/woorifisa/kboxwoori/domain/crawling/service/RankingsCrawlingService.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/crawling/service/RankingsCrawlingService.java
@@ -1,100 +1,79 @@
 package com.woorifisa.kboxwoori.domain.crawling.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.woorifisa.kboxwoori.domain.crawling.dto.RankingsDto;
 import com.woorifisa.kboxwoori.domain.crawling.entity.Rankings;
 import com.woorifisa.kboxwoori.domain.crawling.entity.TeamLogo;
+import com.woorifisa.kboxwoori.domain.crawling.exception.CrawlingDataSaveException;
+import com.woorifisa.kboxwoori.domain.crawling.exception.CrawlingStoredDataNotFoundException;
 import com.woorifisa.kboxwoori.domain.crawling.repository.RankingsRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RankingsCrawlingService {
 
-    private final RedisTemplate<String, String> redisTemplate;
     private final RankingsRepository rankingsRepository;
-    private final ObjectMapper objectMapper;
 
-    private HashOperations<String, String, Rankings> hashOps;
-
-    @PostConstruct
-    private void init() {
-        hashOps = redisTemplate.opsForHash();
-    }
-
-    String formattedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMddHH"));
+    String formattedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
     public void crawlKboRanking() {
         try {
             Document document = Jsoup.connect("https://www.koreabaseball.com/TeamRank/TeamRank.aspx").get();
             Element table = document.select("table.tData").first();
+            if (table == null) {
+                throw CrawlingStoredDataNotFoundException.EXCEPTION;
+            }
+
             Elements rows = table.select("tr");
 
-            int index = 1;
+            int index = 0;
 
             for (int i = 1; i < rows.size(); i++) {
                 Element row = rows.get(i);
                 Elements cells = row.select("td");
 
-                RankingsDto kboRankings = RankingsDto.builder()
-                                                     .id(formattedTime + "-" + index)
-                                                     .rank(cells.get(0).text())
-                                                     .teamName(cells.get(1).text())
-                                                     .teamLogo(TeamLogo.fromString(cells.get(1).text()).getTeamLogoUrl())
-                                                     .gamesPlayed(cells.get(2).text())
-                                                     .wins(cells.get(3).text())
-                                                     .losses(cells.get(4).text())
-                                                     .draws(cells.get(5).text())
-                                                     .winningPercentage(cells.get(6).text())
-                                                     .gameBehind(cells.get(7).text())
-                                                     .build();
-                // DTO에서 Entity로 변환
-                Rankings result = rankingsRepository.save(kboRankings.toEntity());
+                Rankings saveRankings = rankingsRepository.save(Rankings.builder()
+                                                .id(formattedTime + "-" + index)
+                                                .rank(cells.get(0).text())
+                                                .teamName(cells.get(1).text())
+                                                .teamLogo(TeamLogo.fromString(cells.get(1).text()).getTeamLogoUrl())
+                                                .gamesPlayed(cells.get(2).text())
+                                                .wins(cells.get(3).text())
+                                                .losses(cells.get(4).text())
+                                                .draws(cells.get(5).text())
+                                                .winningPercentage(cells.get(6).text())
+                                                .gameBehind(cells.get(7).text())
+                                                .build());
+                log.info("구단 순위 저장 !: " + saveRankings);
                 index++;
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw CrawlingDataSaveException.EXCEPTION;
         }
-
     }
 
-    public String rankingsFindAll() {
-        List<Map<String, Rankings>> allRankingsResults = new ArrayList<>();
-        Set<String> keys = redisTemplate.keys("crawling:rankings:*"); // 모든 관련 키를 찾음
-        System.out.println("keys = " + keys);
-
-        for (String key : keys) {
-            Map<String, Rankings> rankingsMap = hashOps.entries(key);
-            allRankingsResults.add(rankingsMap);
+    public List<Rankings> rankingsFindAll() {
+        List<Rankings> rankingsFindAll = rankingsRepository.findAll(Sort.by("id")).stream()
+                                                    .filter(rankings -> rankings.getId().contains(formattedTime))
+                                                    .collect(Collectors.toList());
+        if (rankingsFindAll.isEmpty()) {
+            throw CrawlingStoredDataNotFoundException.EXCEPTION;
         }
-        String jsonResults = "";
-        try {
-            jsonResults = objectMapper.writeValueAsString(allRankingsResults);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        System.out.println("jsonResults = " + jsonResults);
-        return jsonResults;
+        log.info("구단 순위 API 호출 !: " + rankingsFindAll);
+        return rankingsFindAll;
     }
 }
 
