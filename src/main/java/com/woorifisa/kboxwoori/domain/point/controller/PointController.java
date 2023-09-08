2b2a98a74814c3e04cb392979ea5395bf4e34b9a
Dain Lee <65268003+dain-lee@users.noreply.github.com>
2023-09-09 01:10:29 +0900

    [Feature] 요청을 보낸 사용자의 정보를 가져오기 위한 util 추가 (#104)
    * feat: util 사용 코드 추가
    
    * remove: 주석 제거

--- src/main/java/com/woorifisa/kboxwoori/domain/point/controller/PointController.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/point/controller/PointController.java
@@ -3,11 +3,11 @@ package com.woorifisa.kboxwoori.domain.point.controller;
 import com.woorifisa.kboxwoori.domain.point.dto.PointHistoryDto;
 import com.woorifisa.kboxwoori.domain.point.dto.UserPointResponseDto;
 import com.woorifisa.kboxwoori.domain.point.service.PointService;
-import com.woorifisa.kboxwoori.global.config.security.PrincipalDetails;
 import com.woorifisa.kboxwoori.global.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import static com.woorifisa.kboxwoori.global.util.AuthenticationUtil.getCurrentUserId;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,20 +17,20 @@ public class PointController {
     private final PointService pointService;
 
     @GetMapping
-    public ResponseDto<PointHistoryDto> getPointHistory(@AuthenticationPrincipal PrincipalDetails pdetails){
-        PointHistoryDto pointHistoryDTO = pointService.getPointHistory(pdetails.getUsername());
+    public ResponseDto<PointHistoryDto> getPointHistory(){
+        PointHistoryDto pointHistoryDTO = pointService.getPointHistory(getCurrentUserId());
         return ResponseDto.success(pointHistoryDTO);
     }
 
     @GetMapping("/honeymoney")
-    public ResponseDto<UserPointResponseDto> getUserPoint(@AuthenticationPrincipal PrincipalDetails pdetails){
-        UserPointResponseDto userPointResponseDTO = pointService.getUserPoint(pdetails.getUsername());
+    public ResponseDto<UserPointResponseDto> getUserPoint(){
+        UserPointResponseDto userPointResponseDTO = pointService.getUserPoint(getCurrentUserId());
         return ResponseDto.success(userPointResponseDTO);
     }
 
     @PostMapping("/honeymoney")
-    public ResponseDto usePoint(@AuthenticationPrincipal PrincipalDetails pdetails, @RequestBody UserPointResponseDto userPointResponseDTO){
-        pointService.usePoint(pdetails.getUsername(), userPointResponseDTO);
+    public ResponseDto usePoint(@RequestBody UserPointResponseDto userPointResponseDTO){
+        pointService.usePoint(getCurrentUserId(), userPointResponseDTO);
         return ResponseDto.success();
 
     }
--- src/main/java/com/woorifisa/kboxwoori/domain/prediction/service/PredictionService.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/prediction/service/PredictionService.java
@@ -37,7 +37,6 @@ public class PredictionService {
     }
 
     public void savePrediction(String userId, PredictionRequestDto predictionRequestDto) {
-        //TODO: 모두 예측했는지 확인 (경기 수는 가변값)
         if (predictionRequestDto.getPredictions().size() < predictionRedisRepository.getMatchCount()) {
             throw AllPredictionsRequiredException.EXCEPTION;
         }
