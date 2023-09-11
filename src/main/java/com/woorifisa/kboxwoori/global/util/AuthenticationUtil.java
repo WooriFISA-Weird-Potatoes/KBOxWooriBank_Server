b99f192234ced8bccb852835895a2428f99d8e57
Dain Lee <dain9934@gmail.com>
2023-09-11 15:42:04 +0900

    feat: 권한 반환 util 추가

--- src/main/java/com/woorifisa/kboxwoori/global/util/AuthenticationUtil.java
+++ src/main/java/com/woorifisa/kboxwoori/global/util/AuthenticationUtil.java
@@ -1,8 +1,12 @@
 package com.woorifisa.kboxwoori.global.util;
 
+import com.woorifisa.kboxwoori.domain.user.entity.Role;
 import com.woorifisa.kboxwoori.global.config.security.CustomUserDetails;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Component
 public class AuthenticationUtil {
@@ -16,5 +20,10 @@ public class AuthenticationUtil {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         CustomUserDetails currentUser = (CustomUserDetails) principal;
         return currentUser.isWooriLinked();
+    }
+
+    public static Role getAuthorities() {
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return Role.valueOf(authorities.iterator().next().getAuthority());
     }
 }
