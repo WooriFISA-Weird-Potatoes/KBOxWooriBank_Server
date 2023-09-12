7147a970045a08208b34d1aea7147cabc3bab74c
Dain Lee <dain9934@gmail.com>
2023-09-12 10:36:23 +0900

    fix: 오늘 일정 크롤링 테스트 코드 삭제

--- src/test/java/com/woorifisa/kboxwoori/domain/crawling/service/TodayScheduleCrawlingServiceTest.java
+++ src/test/java/com/woorifisa/kboxwoori/domain/crawling/service/TodayScheduleCrawlingServiceTest.java
@@ -14,20 +14,17 @@ class TodayScheduleCrawlingServiceTest {
     private TodayScheduleCrawlingService service;
 
     @Test
-    @BeforeAll
-    void 오늘일정크롤링저장() {
-        service.crawlKboTodaySchedule();
-    }
-    @Test
     @Order(3)
     void 오늘일정크롤링모두가져오기() {
         service.todayScheduleFindAll();
     }
+
     @Test
     @Order(1)
     void 오늘일정크롤링경기수저장() {
         service.saveTodayMatchCount();
     }
+
     @Test
     @Order(2)
     void 오늘일정크롤링가장빠른시간() {
