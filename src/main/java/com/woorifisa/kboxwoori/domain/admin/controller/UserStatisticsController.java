6fb5ffda9fad3624f4b4af1168c2470c48904e4f
Dain Lee <dain9934@gmail.com>
2023-09-12 17:48:32 +0900

    fix: 각 컬럼 nullable 수정

--- src/main/java/com/woorifisa/kboxwoori/domain/admin/controller/UserStatisticsController.java
+++ src/main/java/com/woorifisa/kboxwoori/domain/admin/controller/UserStatisticsController.java
@@ -7,6 +7,7 @@ import com.woorifisa.kboxwoori.domain.admin.service.UserStatisticsService;
 import com.woorifisa.kboxwoori.domain.event.entity.Event;
 import com.woorifisa.kboxwoori.domain.quiz.entity.Quiz;
 import com.woorifisa.kboxwoori.global.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,13 +16,11 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/admin")
 public class UserStatisticsController {
     private final UserStatisticsService userStatisticsService;
-    @Autowired
-    public UserStatisticsController(UserStatisticsService userStatisticsService) {
-        this.userStatisticsService = userStatisticsService;
-    }
+
     @GetMapping("/users/weekly")
     public ResponseDto<List<UserResponseDto>> getUserStatisticsForLast7Days(){
         return ResponseDto.success(userStatisticsService.getUserStatisticsForLast7Days());
@@ -54,6 +53,7 @@ public class UserStatisticsController {
         List<Quiz> quizList = userStatisticsService.processQuizFileUpload(file);
         return ResponseDto.success(quizList);
     }
+
     @PostMapping("/upload/product")
     public ResponseDto<List<Event>> handleProductFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
         List<Event> eventList = userStatisticsService.processEventFileUpload(file);
