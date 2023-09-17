d8b156e10d9b675e20f8f6b8a43a844b5c8a926f
차은영 <sts07129@naver.com>
2023-09-17 15:43:31 +0900

    deploy: deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 sudo docker pull eycha/kboxwoori:0.1.0
 sudo docker stop gamza-container || true && sudo docker rm gamza-container || true
-sudo docker run --name gamza-container -d -p 80:8080 eycha/kboxwoori:0.1.0
+docker run -d -p 80:8080 -e TZ=Asia/Seoul -v /home/ubuntu/logs:/logs --name gamza-container eycha/kboxwoori:0.1.0
